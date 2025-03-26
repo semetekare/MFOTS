4226721e594313ebab7adeacfa47863600afb644
semetekare <visokihdanil2004@gmail.com>
2025-03-26 14:14:09 +0800

    add websocket

--- MFOTS/MFOTS/asgi.py
+++ MFOTS/MFOTS/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MFOTS.settings')
+django_asgi_app = ASGIStaticFilesHandler(get_asgi_application())
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from .routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(websocket_urlpatterns)
+    ),
+})

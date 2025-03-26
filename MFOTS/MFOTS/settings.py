58d6de5a2fb8566de9b7d07d16998f11309d72fc
semetekare <visokihdanil2004@gmail.com>
2025-03-26 10:19:41 +0800

    fix static

--- MFOTS/MFOTS/settings.py
+++ MFOTS/MFOTS/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 import sys
 from pathlib import Path
 from .env_config import CONFIG
@@ -163,8 +164,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
